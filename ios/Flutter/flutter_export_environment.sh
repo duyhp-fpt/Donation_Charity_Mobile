bc0f5e866f1c7ca9545a1e5842826cc2ad64c7d7
duyhp-fpt <hpduy00vvk@gmail.com>
2021-10-25 08:48:12 +0700

    Full UI donator

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\anphu\developer\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Download\Template-donation"
+export "FLUTTER_ROOT=C:\Users\Administrator\Development\sdks\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Administrator\Desktop\Donation_Charity_Mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
